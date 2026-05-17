6a5477876242374bd263e0ec6c2ad6ee981581f6
Vineeth20-05 <vineethmgowda123@gmail.com>
2026-05-17 20:12:05 +0530

    Implemented resume upload and parsing pipeline

--- django_app/core/settings.py
+++ django_app/core/settings.py
@@ -76,11 +76,14 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hireiq_db',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
--- django_app/recruiter/views.py
+++ django_app/recruiter/views.py
@@ -1,4 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ResumeUploadForm
+from .utils import extract_text_from_pdf, extract_text_from_docx
 
 def home(request):
-    return render(request, 'home.html')
+    if request.method == 'POST':
+        form = ResumeUploadForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            resume = form.save()
+
+            file_path = resume.resume_file.path
+
+            extracted_text = ""
+
+            if file_path.endswith('.pdf'):
+                extracted_text = extract_text_from_pdf(file_path)
+
+            elif file_path.endswith('.docx'):
+                extracted_text = extract_text_from_docx(file_path)
+
+            resume.extracted_text = extracted_text
+
+            resume.save()
+
+            return redirect('home')
+
+    else:
+        form = ResumeUploadForm()
+
+    return render(request, 'home.html', {'form': form})
