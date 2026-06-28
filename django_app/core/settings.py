93a5686af8b494a789832a89830b65e554d8a208
Vineeth20-05 <vineethmgowda123@gmail.com>
2026-06-28 22:45:04 +0530

    Nginx added

--- django_app/core/settings.py
+++ django_app/core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-d9fiy-14(54@0$&vn%(17^s4%7(t8w=nr)dqq)+$cl7p6d$15n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -129,10 +129,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
